5057fd3260c63310fc8b83c93689a33ea4aef923
minhtan16399 <minhtan3718a@gmail.com>
2024-01-24 01:20:00 +0700

    add code homep-age & manage-page

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import homePageRoutes from "./homePageRoutes.js";
+import imageManagementRoutes from "./imageManagementRoutes.js";
 
 const rootRoutes = express();
+
+rootRoutes.use('/images', homePageRoutes);
+rootRoutes.use('/users', imageManagementRoutes);
 
 export default rootRoutes;
